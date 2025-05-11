00a0c02fa762b7bf81c3406f444476f197e2de08
Anton Fayfer <fayfer2004@mail.ru>
2025-05-11 20:02:10 +0600

    fix work with db

--- app/posts/service.py
+++ app/posts/service.py
@@ -20,28 +20,20 @@ class PostService:
         if posts:  # Проверяем, что список не пустой
             await self.post_cache.set_posts(posts)
         return posts
-
     async def create_post(self, body: PostCreateSchema, user_id: int) -> PostSchema:
         post = await self.post_repository.create_post(body, user_id)
         if not post:
             raise HTTPException(status_code=500, detail="Failed to create post")
         return PostSchema.model_validate(post)
-
-    async def update_post_name(
-        self, post_id: int, content: str, user_id: int
-    ) -> PostSchema:
-        post = await self.post_repository.get_user_post(
-            user_id=user_id, post_id=post_id
-        )
+    
+    async def update_post_name(self, post_id: int, content: str, user_id: int) -> PostSchema:
+        post = await self.post_repository.get_user_post(user_id=user_id, post_id=post_id)
         if not post:
             raise PostNotFound
-        updated_post = await self.post_repository.update_post_name(
-            post_id=post_id, content=content
-        )
+        updated_post = await self.post_repository.update_post_name(post_id=post_id, content=content)
         if not updated_post:
             raise PostNotFound
         return PostSchema.model_validate(updated_post)
-
     async def delete_post(self, post_id: int, user_id: int) -> None:
         post = await self.post_repository.get_user_post(
             user_id=user_id, post_id=post_id
